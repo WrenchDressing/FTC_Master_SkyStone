91c08133c56c86510895b23ca7c3e576e0c4cd8e
WrenchDressing <44074046+WrenchDressing@users.noreply.github.com>
2019-09-19 19:12:09 -0500

    Update TeleOp_Mode_Jake.java

--- TeleOp_Mode_Jake.java
+++ TeleOp_Mode_Jake.java
@@ -48,10 +48,10 @@ public class Teleop3 extends LinearOpMode {
         
           //motor gamestick algorithm
           
-        motor_drive_fl.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x) * slowerMode);
-        motor_drive_bl.setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x) * slowerMode);
-        motor_drive_fr.setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x) * slowerMode);
-        motor_drive_br.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x)* slowerMode);
+        int fl = (gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x) * slowerMode;
+        int bl = (-gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x) * slowerMode;
+        int fr = (-gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x) * slowerMode;
+        int br = (gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x)* slowerMode;
                                       
           //right trigger flips motors
           
@@ -62,119 +62,48 @@ public class Teleop3 extends LinearOpMode {
           motor_drive_bl.setPower(-motor_drive_bl.getPower());
         }
           
-        /*  //gamepad x and b runs collections servos
-          
-        if (gamepad2.x == true) {
-          collection_servo.setPower(-1);
-          collection_servo2.setPower(1);
-        } else if (gamepad2.b == true) {
-          collection_servo.setPower(1);
-          collection_servo2.setPower(-1);
+       private void normalizationDrive(int fl, int bl, int fr, int br) {
+          if(Math.abs(fl) >= Math.abs(bl) && Math.abs(fl) >= Math.abs(fr) && Math.abs(fl) >= Math.abs(br)){
+            motor_drive_fl.setPower(fl / Math.abs(fl));
+            motor_drive_fr.setPower(fr / Math.abs(fl));
+            motor_drive_bl.setPower(bl / Math.abs(fl));
+            motor_drive_br.setPower(br / Math.abs(fl));
           }
-            else {
-              collection_servo2.setPower(.055);
-              collection_servo.setPower(.055);
-            }
-            
-          //hook mechanism code using bumpers
-
-        if (gamepad2.left_bumper == false && gamepad2.right_bumper == false) {
-          motor_alt_pull.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-          motor_alt_pull.setTargetPosition(Current_Val3);
-          motor_alt_pull.setPower(1); 
-        } else {
-          motor_alt_pull.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-          if (gamepad2.left_bumper == true){
-            motor_alt_pull.setPower(.25);
-          } else if (gamepad2.right_bumper == true) {
-              motor_alt_pull.setPower(-.25);
-            }
-            Current_Val3 = motor_alt_pull.getCurrentPosition();
-        }
-
-          //gamepad 2 a and y for running elbow code, inlcuding l3 and r3 for auto positions
-
-        if (gamepad2.a == false && gamepad2.y == false) {
-          arm_motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-          if (cancel1) {
-          Current_Val2 = arm_motor2.getCurrentPosition();
-          }
-          if (gamepad2.left_stick_button == true) {
-            arm_motor2.setTargetPosition(-25);
-            arm_motor2.setPower(0.25);
-            cancel1 = true;
-          }
-          else if (gamepad2.right_stick_button == true) {
-            arm_motor2.setTargetPosition(875);
-            arm_motor2.setPower(0.15);
-            cancel1 = true;
-          }
-          else {
-          arm_motor2.setTargetPosition(Current_Val2);
-          cancel1 = false;
-          arm_motor2.setPower(1);
-          }
-        } 
-          else {
-            arm_motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            cancel1 = true;
-            if (gamepad2.a == true){
-              arm_motor2.setPower(-.10);
-            } else if (gamepad2.y == true) {
-              arm_motor2.setPower(.25);
-              }
-            }
-            
-            //gamepad 2 dpad up and down for running arm code, inlcuding l3 and r3 auto positions
-            
-        if (gamepad2.dpad_down == false && gamepad2.dpad_up == false) {
-          arm_motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-          if (cancel2) {
-          Current_Val = arm_motor1.getCurrentPosition();
-          }
-          if (gamepad2.left_stick_button == true) {
-            arm_motor1.setTargetPosition(-1500);
-            arm_motor1.setPower(0.20);
-            cancel2 = true;
-          }
-          else if (gamepad2.right_stick_button == true) {
-            arm_motor1.setTargetPosition(-650);
-            arm_motor1.setPower(0.10);
-            cancel2 = true;
-          }
-          else {
-          arm_motor1.setTargetPosition(Current_Val);
-          cancel2 = false;
-          arm_motor1.setPower(1);
-          }
-        } else {
-          arm_motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-          cancel2 = true;
-          if (gamepad2.dpad_down == true) {
-            arm_motor1.setPower(0.10);
-          } else if (gamepad2.dpad_up == true) {
-            arm_motor1.setPower(-0.15);
-          }
-        } */
+         else if(Math.abs(bl) >= Math.abs(fl) && Math.abs(bl) >= Math.abs(fr) && Math.abs(bl) >= Math.abs(br)){
+            motor_drive_fl.setPower(fl / Math.abs(bl));
+            motor_drive_fr.setPower(fr / Math.abs(bl));
+            motor_drive_bl.setPower(bl / Math.abs(bl));
+            motor_drive_br.setPower(br / Math.abs(bl));
+         }
+         else if(Math.abs(fr) >= Math.abs(fl) && Math.abs(fr) >= Math.abs(bl) && Math.abs(fr) >= Math.abs(br)){
+            motor_drive_fl.setPower(fl / Math.abs(fr));
+            motor_drive_fr.setPower(fr / Math.abs(fr));
+            motor_drive_bl.setPower(bl / Math.abs(fr));
+            motor_drive_br.setPower(br / Math.abs(fr));
+         }
+         else if(Math.abs(br) >= Math.abs(bl) && Math.abs(br) >= Math.abs(fr) && Math.abs(br) >= Math.abs(fl)){
+            motor_drive_fl.setPower(fl / Math.abs(br));
+            motor_drive_fr.setPower(fr / Math.abs(br));
+            motor_drive_bl.setPower(bl / Math.abs(br));
+            motor_drive_br.setPower(br / Math.abs(br));
+         }
+       }
+        
+       private int AbsoluteVal (int num){
+         if(num > 0){
+           return num;
+         }
+         else if(num < 0){
+           return num * -1;
+         }
+         else{
+           return num;
+         }
+       }
         
           telemetry.addLine("Tele-Op is Green!");
-          //telemetry.addData("Target Arm Position", Current_Val);
-          //telemetry.addData("Target Elbow Position", Current_Val2);
           telemetry.update();
         
       }  
     }
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
+  } 

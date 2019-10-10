aac592b2f913e9d6ca1c2ad8687dda262642ea1f
WrenchDressing <44074046+WrenchDressing@users.noreply.github.com>
2019-10-10 18:39:52 -0500

    Update Auto_Mode_Jake.java

--- Auto_Mode_Jake.java
+++ Auto_Mode_Jake.java
@@ -1,6 +1,15 @@
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;            
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="Auto_Mode_Jake.java", group="Pushbot")
+@Disabled
+public class Auto_Mode_Jake.java extends LinearOpMode {
+}

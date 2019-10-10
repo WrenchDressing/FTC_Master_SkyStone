5b93e0b208a74aa0b6f6d5afd0d623422b1346ae
WrenchDressing <44074046+WrenchDressing@users.noreply.github.com>
2019-10-10 18:37:12 -0500

    Update and rename Auto_Mode_Jake to Auto_Mode_Jake.java

---
+++ Auto_Mode_Jake.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
